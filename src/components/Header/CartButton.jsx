395ec469f6062d026697aa6d65e924360f806c3f
mathesukkj <kemuel.g7363@gmail.com>
2023-04-07 01:24:12 -0300

    feat: add cartbutton dynamic amount

--- src/components/Header/CartButton.jsx
+++ src/components/Header/CartButton.jsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import styled from "styled-components";
+import CartContext from "../../context/cart-context";
 
 const Container = styled.button`
     padding: 1rem;
@@ -23,6 +25,12 @@ const ItemsCount = styled.div`
 `;
 
 export default function CartButton(props) {
+    const context = useContext(CartContext);
+
+    const totalItems = context.items.reduce((total, item) => {
+        return total + item.amount;
+    }, 0);
+
     return (
         <Container onClick={props.onClick}>
             <svg
@@ -35,7 +43,7 @@ export default function CartButton(props) {
                 <path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z" />
             </svg>
             Your cart
-            <ItemsCount>0</ItemsCount>
+            <ItemsCount>{totalItems}</ItemsCount>
         </Container>
     );
 }
